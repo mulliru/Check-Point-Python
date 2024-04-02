e45f1f843c9b94f0f3328796f12879cf5bce0fd6
Murillo Ferreira <murilloferreira348@gmail.com>
2024-04-01 23:37:22 -0300

    Adicionando na classe de ordenacao o metodo bubble sort

--- proj-algoritmos/proj-algoritmos/algoritmos/ordenacao.py
+++ proj-algoritmos/proj-algoritmos/algoritmos/ordenacao.py
@@ -1,1 +1,14 @@
-# implemente aqui....
+#NOSSO ARQUIVOOO PODE FAZER COMMIT 
+
+class Ordenacao:
+    
+    ## bubble sort
+    @staticmethod
+    def bubble_sort(lista):
+        n = len(lista)
+        for i in range(n):
+            for j in range(0, n-i-1):
+                if lista[j] > lista[j+1]:
+                    lista[j], lista[j+1] = lista[j+1], lista[j]
+        return lista
+    
