c046fca818591f3fd198f1e38235aa684062296f
Larissa L. Oliveira <91492813+worklarissa@users.noreply.github.com>
2024-04-02 03:05:11 +0000

    Adicionando na classe de ordenacao o metodo insertion sort

--- proj-algoritmos/proj-algoritmos/algoritmos/ordenacao.py
+++ proj-algoritmos/proj-algoritmos/algoritmos/ordenacao.py
@@ -23,3 +23,15 @@ class Ordenacao:
                     menor_indice = j
             lista[i], lista[menor_indice] = lista[menor_indice], lista[i]
         return lista
+
+    ## insertion sort
+    @staticmethod
+    def insertion_sort(lista):
+        for i in range(1, len(lista)):
+            chave = lista[i]
+            j = i-1
+            while j >= 0 and lista[j] > chave:
+                lista[j + 1] = lista[j]
+                j -= 1
+            lista[j + 1] = chave
+        return lista
