cd13293dbac188a240bc632e83b0b1ca5382491b
Murillo Ferreira <murilloferreira348@gmail.com>
2024-04-02 00:15:16 -0300

    Adicionando na classe de ordenacao o metodo merge sort atualizado

--- proj-algoritmos/proj-algoritmos/algoritmos/ordenacao.py
+++ proj-algoritmos/proj-algoritmos/algoritmos/ordenacao.py
@@ -36,16 +36,29 @@ class Ordenacao:
             lista[j + 1] = chave
         return lista
     
-        ## merge sort
+    ## merge sort
     @staticmethod
     def merge_sort(lista):
         if len(lista) <= 1:
             return lista
+        
         meio = len(lista) // 2
         esquerda = lista[:meio]
         direita = lista[meio:]
-        
+
         esquerda = Ordenacao.merge_sort(esquerda)
         direita = Ordenacao.merge_sort(direita)
         
-        return Ordenacao.merge(esquerda, direita)
+        resultado = []
+        i = j = 0
+        while i < len(esquerda) and j < len(direita):
+            if esquerda[i] < direita[j]:
+                resultado.append(esquerda[i])
+                i += 1
+            else:
+                resultado.append(direita[j])
+                j += 1
+
+        resultado.extend(esquerda[i:])
+        resultado.extend(direita[j:])
+        return resultado
