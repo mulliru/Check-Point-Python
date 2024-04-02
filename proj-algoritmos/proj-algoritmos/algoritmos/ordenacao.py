4a832372483af55069cbfaa2ee3452f4275d23c2
Pedro Luiz <142911409+PedroLuizPrado@users.noreply.github.com>
2024-04-02 02:52:44 +0000

    Adicionando na classe de ordenacao o metodo selection sort

--- proj-algoritmos/proj-algoritmos/algoritmos/ordenacao.py
+++ proj-algoritmos/proj-algoritmos/algoritmos/ordenacao.py
@@ -11,4 +11,15 @@ class Ordenacao:
                 if lista[j] > lista[j+1]:
                     lista[j], lista[j+1] = lista[j+1], lista[j]
         return lista
-    
+
+    ## selection sort
+    @staticmethod
+    def selection_sort(lista):
+        n = len(lista)
+        for i in range(n):
+            menor_indice = i
+            for j in range(i+1, n):
+                if lista[j] < lista[menor_indice]:
+                    menor_indice = j
+            lista[i], lista[menor_indice] = lista[menor_indice], lista[i]
+        return lista
